daaaed0682d750d64e592166f91bbd187efa90f5
MarrMika <slobodjanmarija1@gmail.com>
2019-10-04 21:22:12 +0300

    Services and Repositories have been done.

--- src/main/java/com/example/demo/repositories/BlockingRepository.java
+++ src/main/java/com/example/demo/repositories/BlockingRepository.java
@@ -1,4 +1,8 @@
 package com.example.demo.repositories;
 
-public interface BlockingRepository {
+import com.example.demo.domains.Blocking;
+import com.example.demo.domains.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlockingRepository extends JpaRepository<Blocking,String> {
 }
--- src/main/java/com/example/demo/repositories/UserRepository.java
+++ src/main/java/com/example/demo/repositories/UserRepository.java
@@ -3,6 +3,6 @@ package com.example.demo.repositories;
 import com.example.demo.domains.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User,String> {
 
 }
